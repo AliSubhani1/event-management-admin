93222f714b67edad79254f189a484e1ff9c0bc2b
Ali <ali.subhani@tintash.com>
2024-10-29 22:11:47 +0200

    created all required necessary folders and added initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1 className="text-3xl font-bold underline text-red-400">
-    Hello world!
-  </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Dashboard />
       </header>
     </div>
   );
